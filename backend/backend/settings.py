281f5cf755ff9b9a4bead37d065f6092aa22812c
ViktoroUrso <viktoro.urso@gmail.com>
2023-09-01 23:47:37 +0300

    Correct settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9'
-'_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', '158.160.0.50']
 
 # Application definition
 
@@ -74,13 +72,12 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.'
-        'password_validation.UserAttributeSimilarityValidator',
-        },
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
     {
         'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
+    },
     {
         'NAME':
         'django.contrib.auth.password_validation.CommonPasswordValidator',
